320d6a0bad82327ea04f5a12631ae2a402614bf2
Kyle M Hall <kyle@bywatersolutions.com>
2020-10-05 11:57:51 -0400

    Tidy code using Black

--- aspen_exporter.py
+++ aspen_exporter.py
@@ -16,50 +16,57 @@ import urllib
 
 from collector import collect_aspen
 
+
 class ForkingHTTPServer(ForkingMixIn, HTTPServer):
-  pass
+    pass
+
 
 class AspenExporterHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         url = urlparse(self.path)
-        if url.path == '/metrics':
-          params = urllib.parse.parse_qs(url.query)
-          if 'address' not in params:
-            self.send_response(400)
+        if url.path == "/metrics":
+            params = urllib.parse.parse_qs(url.query)
+            if "address" not in params:
+                self.send_response(400)
+                self.end_headers()
+                self.wfile.write("Parameter 'address' is required".encode("utf-8"))
+                return
+
+            self.send_response(200)
+            self.send_header("Content-Type", CONTENT_TYPE_LATEST)
             self.end_headers()
-            self.wfile.write("Parameter 'address' is required".encode('utf-8'))
-            return
 
-          self.send_response(200)
-          self.send_header('Content-Type', CONTENT_TYPE_LATEST)
-          self.end_headers()
+            address = params["address"][0]
+            output = collect_aspen(address)
 
-          address = params['address'][0]
-          output = collect_aspen(address)
-
-          self.wfile.write(output)
-        elif url.path == '/':
-          self.send_response(200)
-          self.end_headers()
-          self.wfile.write("""<html>
+            self.wfile.write(output)
+        elif url.path == "/":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(
+                """<html>
           <head><title>Aspen Exporter for Prometheus</title></head>
           <body>
           <h1>Aspen Exporter for Prometheus</h1>
           <p>Visit <code>/metrics?address=my.aspen.example.com</code> to use.</p>
           </body>
-          </html>""".encode('utf-8'))
+          </html>""".encode(
+                    "utf-8"
+                )
+            )
         else:
-          self.send_response(404)
-          self.end_headers()
+            self.send_response(404)
+            self.end_headers()
+
 
 def run(server_class=HTTPServer, handler_class=AspenExporterHandler, cli_port=""):
-    port = cli_port or os.environ.get('ASPEN_EXPORTER_PORT') or 9750
+    port = cli_port or os.environ.get("ASPEN_EXPORTER_PORT") or 9750
     port = int(port)
 
     logging.basicConfig(level=logging.INFO)
-    logging.info(f'Starting httpd on port {port}...\n')
+    logging.info(f"Starting httpd on port {port}...\n")
 
-    server_address = ('', port)
+    server_address = ("", port)
     httpd = server_class(server_address, handler_class)
 
     try:
@@ -67,9 +74,10 @@ def run(server_class=HTTPServer, handler_class=AspenExporterHandler, cli_port=""
     except KeyboardInterrupt:
         pass
     httpd.server_close()
-    logging.info('Stopping httpd...\n')
+    logging.info("Stopping httpd...\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     from sys import argv
 
     if len(argv) == 2:
--- collector.py
+++ collector.py
@@ -3,10 +3,12 @@ from prometheus_client.core import GaugeMetricFamily
 import requests
 import logging
 
+
 def collect_aspen(address):
     registry = CollectorRegistry()
     registry.register(CustomCollector(address))
     return generate_latest(registry)
+
 
 class CustomCollector(object):
     def __init__(self, address):
@@ -24,18 +26,18 @@ class CustomCollector(object):
         data = response.json()
 
         # Aspen general health status
-        aspen_health_status = data["result"]["aspen_health_status"];
+        aspen_health_status = data["result"]["aspen_health_status"]
 
         if aspen_health_status == "okay":
             is_ok = 1
         elif aspen_health_status == "warning":
-            is_ok = .5
-        else: # critical
+            is_ok = 0.5
+        else:  # critical
             is_ok = 0
 
         metric_name = "aspen_check_health_status".replace(".", "_")
         metric_name.replace(".", "_")
-        ok = GaugeMetricFamily(metric_name, f'Is aspen ok', labels=['instance'])
+        ok = GaugeMetricFamily(metric_name, f"Is aspen ok", labels=["instance"])
         ok.add_metric([aspen_fqdn], is_ok)
 
         metrics[metric_name] = ok
@@ -48,33 +50,39 @@ class CustomCollector(object):
             if status == "okay":
                 is_ok = 1
             elif status == "warning":
-                is_ok = .5
-            else: # critical
+                is_ok = 0.5
+            else:  # critical
                 is_ok = 0
 
             metric_name = f"aspen_check_{key}".replace(".", "_")
-            ok = GaugeMetricFamily(metric_name, f'Is {key} ok', labels=['instance','aspen_health_check_type'])
-            ok.add_metric([aspen_fqdn,key], is_ok)
+            ok = GaugeMetricFamily(
+                metric_name,
+                f"Is {key} ok",
+                labels=["instance", "aspen_health_check_type"],
+            )
+            ok.add_metric([aspen_fqdn, key], is_ok)
 
             metrics[metric_name] = ok
 
         # Aspen server metrics
         for key in data["result"]["serverStats"].keys():
             val = str(data["result"]["serverStats"][key]["value"])
-            vals = val.split(" ");
+            vals = val.split(" ")
             metric_name = f"aspen_check_{key}".replace(".", "_")
             metric_name.replace(".", "_")
-            if len(vals) > 1: # Value contains a number and a metric, i.e. "41.49 GB"
+            if len(vals) > 1:  # Value contains a number and a metric, i.e. "41.49 GB"
                 val = vals[0]
                 metric = vals[1]
                 desc = data["result"]["serverStats"][key]["name"]
-                ok = GaugeMetricFamily(metric_name, f'{desc} in {metric}', labels=['instance'])
+                ok = GaugeMetricFamily(
+                    metric_name, f"{desc} in {metric}", labels=["instance"]
+                )
                 ok.add_metric([aspen_fqdn], val)
                 metrics[metric_name] = ok
-            else: # Value is just a simple number, i.e. "0.31" # Value is just a simple number, i.e. "0.31"
+            else:  # Value is just a simple number, i.e. "0.31" # Value is just a simple number, i.e. "0.31"
                 val = data["result"]["serverStats"][key]["value"]
                 desc = data["result"]["serverStats"][key]["name"]
-                ok = GaugeMetricFamily(f"aspen_stat_{key}", desc, labels=['instance'])
+                ok = GaugeMetricFamily(f"aspen_stat_{key}", desc, labels=["instance"])
                 ok.add_metric([aspen_fqdn], val)
                 metrics[metric_name] = ok
         return metrics.values()
